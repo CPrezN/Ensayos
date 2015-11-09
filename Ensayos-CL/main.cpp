f060b3dc39bb796030bcc2ef8631eb9fe131d810
Carlos Pérez <u341349@macbook-air-de-carlos-perez.local>
2015-11-09 18:45:37 +0100

    Initial Commit

---
+++ Ensayos-CL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ensayos-CL
+//
+//  Created by Carlos Pérez on 9/11/15.
+//  Copyright © 2015 Carlos Pérez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
