a76c7de8d35c49aa4cacdc6ce61a8c88534d9770
Carlos Pérez <u341349@macbook-air-de-carlos-perez.local>
2015-11-09 19:11:38 +0100

    Pruebecilla sumando

--- Ensayos-CL/main.cpp
+++ Ensayos-CL/main.cpp
@@ -9,7 +9,22 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    int num1 = 0;
+    int num2 = 0;
+    int result = 0;
+    
     std::cout << "Hello, World!\n";
+    
+    std::cout << "Hola Carlos, introduce un número" << std::endl;
+    std::cin >> num1;
+    
+    std::cout << "Ahora introduce otro número" << std::endl;
+    std::cin >> num2;
+    
+    result = num1 + num2;
+    std::cout << "El resultado de " << num1 <<" + " << num2 << " es: " << result << std::endl;
+    std::cout << "Adios!!!" << std::endl;
+    
     return 0;
 }
